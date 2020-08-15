af81947c106846f260d5849e1473476fc913923b
VuntA <40677857+VuntA@users.noreply.github.com>
2020-08-15 10:01:55 -0500

    Update BiomeCheck.java

--- Starfall/src/me/starfall/biome/BiomeCheck.java
+++ Starfall/src/me/starfall/biome/BiomeCheck.java
@@ -6,11 +6,7 @@ import org.bukkit.block.Biome;
 public class BiomeCheck {
     public static BiomeCheck plugin;
 
-    public static boolean isOceanBiome(Location loc, Biome b) {
+    public static boolean checkBiome(Location loc, Biome b) {
         return loc.getWorld().getBiome(loc.getBlockX(), loc.getBlockZ()) == b;
-    }
-
-    public static boolean isDeepOceanBiome(Location loc, Biome c) {
-        return loc.getWorld().getBiome(loc.getBlockX(), loc.getBlockZ()) == c;
     }
 }
